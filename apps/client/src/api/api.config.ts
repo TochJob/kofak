a94a25d72f0f2bd98f534ba4f2035615bad9b9dc
TochJob <tochjob@gmail.com>
2025-06-12 15:45:28 +0300

    update: config

--- apps/client/src/api/api.config.ts
+++ apps/client/src/api/api.config.ts
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const API_URL_LIST = {
   apiLogin: `${API_URL}/auth/login/`,
-  apiRegister: `${API_URL}/register`,
+  apiRegister: `${API_URL}/register/`,
 
   apiGetProcessos: `${API_URL}/processes/`,
 };
